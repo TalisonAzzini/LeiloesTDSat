f970e57c503150a8b731bfec58c0e2875d0fe169
Talison Azzini <talison.azzini@gmail.com>
2024-10-27 02:27:22 -0300

    Função de cadastrar produtos no banco de dados

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,33 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
+import java.sql.SQLException;
 
 public class ProdutosDAO {
-    
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        conn = new conectaDAO().connectDB();
+        String sql = "INSERT INTO produtos(nome, valor, status) VALUES (?,?,?)";
+        try {
+            prep = this.conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, "A venda");
+            prep.execute();
+        }
+        catch(SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao registrar.");
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
